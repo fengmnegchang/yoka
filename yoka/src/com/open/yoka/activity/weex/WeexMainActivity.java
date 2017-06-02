29f038327fdf67f54e43d9fa3b57b908d641e0bb
fengmnegchang <624926379@qq.com>
2017-06-02 17:15:38 +0800

    主页透明

--- yoka/src/com/open/yoka/activity/weex/WeexMainActivity.java
+++ yoka/src/com/open/yoka/activity/weex/WeexMainActivity.java
@@ -23,14 +23,28 @@ package com.open.yoka.activity.weex;
  *****************************************************************************************************************************************************************************
  */
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewCompat;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 
+import com.open.yoka.R;
 import com.open.yoka.utils.UrlUtils;
 import com.taobao.weex.IWXRenderListener;
 import com.taobao.weex.WXSDKInstance;
@@ -87,11 +101,77 @@ public class WeexMainActivity extends  FragmentActivity implements IWXRenderList
 		// WXRenderStrategy.APPEND_ASYNC);
     }
 
-    @Override
-    public void onViewCreated(WXSDKInstance instance, View view) {
-        setContentView(view);
-    }
+    @SuppressLint({ "ResourceAsColor", "NewApi" }) @Override
+	public void onViewCreated(WXSDKInstance instance, View view) {
+		try {
+			
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				Window window = getWindow();
+				// 取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
+				window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+				// 需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+				window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+				// 设置状态栏颜色
+				window.setStatusBarColor(getResources().getColor(R.color.status_bar_color));
+				ViewGroup mContentView = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
+				View mChildView = mContentView.getChildAt(0);
+				if (mChildView != null) {
+					// 注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView
+					// 的第一个子 View . 预留出系统 View 的空间.
+					ViewCompat.setFitsSystemWindows(mChildView, true);
+				}
+			}
 
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+				ViewGroup contentView = (ViewGroup) findViewById(android.R.id.content);
+				View statusBarView = new View(this);
+				ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(this));
+				statusBarView.setBackgroundColor(getResources().getColor(R.color.status_bar_color));
+				contentView.addView(statusBarView, lp);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		LayoutParams lp = new LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+		lp.gravity = Gravity.TOP | Gravity.LEFT;
+		if (VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+			int statusBarHeight = getStatusBarHeight(this);
+			lp.topMargin = statusBarHeight;
+		}
+		addContentView(view, lp);
+	}
+    /**
+	 * 得到状态栏高度
+	 * 
+	 * @return
+	 */
+	public static int getStatusBarHeight(Activity act) {
+		/*
+		 * 方法一，荣耀3c无效 Rect frame = new Rect(); act.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame); int statusBarHeight = frame.top; return statusBarHeight;
+		 */
+
+		/*
+		 * 方法二，荣耀3c无效 Rect rectgle= new Rect(); Window window= act.getWindow(); window.getDecorView().getWindowVisibleDisplayFrame(rectgle); int StatusBarHeight= rectgle.top; int contentViewTop=
+		 * window.findViewById(Window.ID_ANDROID_CONTENT).getTop(); int statusBar = contentViewTop - StatusBarHeight; return statusBar;
+		 */
+		// 方法三，荣耀3c有效
+		Class<?> c = null;
+		Object obj = null;
+		Field field = null;
+		int x = 0, sbar = 0;
+		try {
+			c = Class.forName("com.android.internal.R$dimen");
+			obj = c.newInstance();
+			field = c.getField("status_bar_height");
+			x = Integer.parseInt(field.get(obj).toString());
+			sbar = act.getResources().getDimensionPixelSize(x);
+			return sbar;
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		return 0;
+	}
     @Override
     public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
 
